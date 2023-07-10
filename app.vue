92c32987d4a33f768fdaa5adf24446d32bd5b5f0
nirmalmhjr <nirmalmhjr@gmail.com>
2023-07-10 11:27:42 +0545

    side Navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <NuxtWelcome  />
+  <div >
+    <div class=" text-white bg-black w-full h-[100vh]">
+      <div id="sideNav" class=" bg-white w-28 relative">
+        <div  class="flex item-center">
+          <img src="./public/images/netflix-logo.png" alt="" class="h-[70px] w-[50px] ">
+
+          <Icon name="mdi:television" color="black" size="40"/>
+        </div>
+        <div id="sideNav">
+          <Home fillColor="white" size="40"/>
+        </div>
+      </div>
+
+      <div>
+
+        <MovieDetails />
+      </div>
+
+      <div>
+
+        <VideoCarousel />
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+
+</script>
