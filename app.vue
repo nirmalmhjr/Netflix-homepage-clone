59bd2f3ce751543d5e313bb6f1a94aac1338147f
nirmalmhjr <nirmalmhjr@gmail.com>
2023-07-16 19:45:41 +0545

    movies slider added

--- app.vue
+++ app.vue
@@ -1,16 +1,29 @@
 <template>
   <div >
-    <div class=" text-white bg-black w-full h-[100vh]">
-      <div id="sideNav" class=" bg-white w-28 relative">
-        <div  class="flex item-center">
-          <img src="./public/images/netflix-logo.png" alt="" class="h-[70px] w-[50px] ">
-
-          <Icon name="mdi:television" color="black" size="40"/>
-        </div>
-        <div id="sideNav">
-          <Home fillColor="white" size="40"/>
-        </div>
+    <div class=" text-white bg-black w-full h-screen">
+      <div id="sideNav" class=" w-[120px] relative flex item-center">
+          <img src="./public/images/netflix-logo.png" alt="" class=" w-[35px] absolute top-0  mt-5 ml-10">
       </div>
+      
+        <div class="py-2 px-10 mt-28">
+          <Icon name="mdi:magnify" color="white" size="40"/>
+        </div>
+        <div class="py-2 px-10 my-6">
+          <Icon name="mdi:home-outline" color="white" size="40"/>
+        </div>
+        <div class="py-2 px-10 my-6">
+          <Icon name="mdi:trending-up" color="white" size="40"/>
+        </div>
+        <div class="py-2 px-10 my-6">
+          <Icon name="mdi:television" color="white" size="40"/>
+        </div>
+        <div class="py-2 px-10 my-6">
+          <Icon name="mdi:movie-outline" color="white" size="40"/>
+        </div>
+        <div class="py-2 px-10 my-6">
+          <Icon name="mdi:plus" color="white" size="40"/>
+        </div>
+       
 
       <div>
 
